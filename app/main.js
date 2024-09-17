ac679dc724f5746c5aed620d0bbc7477a4eb65a8
João <levelupdojoao@hotmail.com>
2024-09-16 21:53:49 -0300

    fix name of disciplinaRoutes

--- app/main.js
+++ app/main.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const app = express();
 const professorRoutes = require('../routes/professor');
 const alunoRoutes = require('../routes/aluno');
+const disciplinaRoutes = require("../controllers/disciplinaController");
 const port = 3000;
 
 app.use(cors());
@@ -10,6 +11,7 @@ app.use(express.json());
 
 app.use('/api', professorRoutes);
 app.use('/api', alunoRoutes);
+app.use('/api', disciplinaRoutes);
 
 app.listen(port, () => {
     console.log("Server is running on port " + port); 
